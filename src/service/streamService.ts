11972c1bb3961eca47ba5ddba5294b0cb2936da3
Marqu1n <marcos_felipe_123@hotmail.com>
2024-10-29 23:27:45 -0300

    ajustes no fluxo do websocket e manutenção geral

--- src/service/streamService.ts
+++ src/service/streamService.ts
@@ -2,28 +2,47 @@ import { io } from "socket.io-client";
 import { EventEmitter } from "events";
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
-
 const socket = io(API_URL)
+let shouldBeConnected:boolean = true
+export const imagemRetornoEventEmitter:EventEmitter = new EventEmitter(); 
 
 socket.on('connect',()=>{
+    shouldBeConnected = true
     console.log('conectou')
+})
+socket.on('disconnect', ()=>{
+    if(!shouldBeConnected){
+        socket.open()
+    }
+})
+imagemRetornoEventEmitter.on('isConnected',()=>{
+    if(!socket.connect){
+        if(shouldBeConnected){
+            socket.open()
+        }
+    }
+    if(!shouldBeConnected){
+        socket.disconnect()
+    }
 })
 socket.on('imagemRetorno', (imgBytes:ArrayBuffer) => {
     imagemRetornoEventEmitter.emit('retorno',  `data:image/png;base64,${imgBytes}`)
 });
 
-export const imagemRetornoEventEmitter:EventEmitter = new EventEmitter(); 
-
-export const sendImage = async (ms:string) => {
-    if(!socket.connected){
+export const sendImage = async (ms:string, prePos: string | null | undefined) => {
+    if(!socket.connected && shouldBeConnected){
         socket.connect()
     }
+    if(!shouldBeConnected){
+        socket.disconnect()
+    }
     if(ms && ms.length > 10){
-        socket.emit('liveFeed', ms);
+        socket.emit('liveFeed', ms,prePos);
     }
 };
 export const closeSocket = async()=>{
     if (socket.connected) {
+        shouldBeConnected = false
         socket.disconnect();
         console.log('Conexão fechada');
     }
