d9deba140987718da61a30c6fe8cabeef3157aae
Marqu1n <marcos_felipe_123@hotmail.com>
2024-10-30 23:00:07 -0300

    ajuste compressão

--- src/service/streamService.ts
+++ src/service/streamService.ts
@@ -1,48 +1,37 @@
 import { io } from "socket.io-client";
+import { Fetch, WebSocket, WebTransport } from "engine.io-client";
 import { EventEmitter } from "events";
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
-const socket = io(API_URL)
-let shouldBeConnected:boolean = true
+const socket = io(API_URL,{transports:[Fetch,WebSocket],perMessageDeflate:{threshold:2048}})
 export const imagemRetornoEventEmitter:EventEmitter = new EventEmitter(); 
 
 socket.on('connect',()=>{
-    shouldBeConnected = true
     console.log('conectou')
 })
-socket.on('disconnect', ()=>{
-    if(!shouldBeConnected){
-        socket.open()
+socket.on("connect_error", (error) => {
+    if (socket.active) {
+      // temporary failure, the socket will automatically try to reconnect
+    } else {
+      // the connection was denied by the server
+      // in that case, `socket.connect()` must be manually called in order to reconnect
+      console.log(error.message);
     }
-})
-imagemRetornoEventEmitter.on('isConnected',()=>{
-    if(!socket.connect){
-        if(shouldBeConnected){
-            socket.open()
-        }
-    }
-    if(!shouldBeConnected){
-        socket.disconnect()
-    }
-})
+  });
+  socket.on("disconnect", (reason, details) => {
+    console.log("ERRO",details,reason)
+  });
 socket.on('imagemRetorno', (imgBytes:ArrayBuffer) => {
     imagemRetornoEventEmitter.emit('retorno',  `data:image/png;base64,${imgBytes}`)
 });
 
 export const sendImage = async (ms:string, prePos: string | null | undefined) => {
-    if(!socket.connected && shouldBeConnected){
-        socket.connect()
-    }
-    if(!shouldBeConnected){
-        socket.disconnect()
-    }
     if(ms && ms.length > 10){
         socket.emit('liveFeed', ms,prePos);
     }
 };
 export const closeSocket = async()=>{
     if (socket.connected) {
-        shouldBeConnected = false
         socket.disconnect();
         console.log('Conexão fechada');
     }
